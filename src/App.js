aa92977e0b791e385b894251870683eff09747c8
Tobi <erkintotonov@gmail.com>
2024-04-11 17:05:23 +0600

    fixed about page

--- src/App.js
+++ src/App.js
@@ -1,68 +1,17 @@
 import './global.css'
 import Header from './components/Header/Header';
 import HeroBlock from './components/HeroBlock/HeroBlock';
+import AboutSection from './components/AboutSection/AboutSection';
 
 function App() {
   return (
     <div className="App">
-      <div className='main-collector'>
+      <div className='header-collector'>
         <Header /> 
         <HeroBlock />
       </div>
-      <div className='about-section'>
-        <div className='about-section-content'>
-          <h1 className='about-section-h1'>KWS Studio</h1>
-          <p className='about-section-description'>
-            Делаем красивые дизайны для бизнеса, разрабатываем сайты, выводим бренды на рынок. <br /> Наша компания предлагает Вам следующие услуги:
-          </p>
-          <div className='about-section-row-1'>
-            <div className='about-section-collector-1'>
-              <img className='about-section-icon' src='https://s3-alpha-sig.figma.com/img/ec62/1446/941485328270f1bc1016cc7f486ba5d8?Expires=1713744000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=H5I4azuA0T68KgTyOBMEZLy45jeBvPs6q3USlXaInPdDZHzzaq8BQ3lgCqkg1H6aPIGJYhUayMOTmjoWmotfoI2ToGWXgOqIegoADugwigRdy2wNCb5beWZlW4Db93GtFXnU4GxdHbsr3CZcKWFkgp48uwMyV1Lm-W5xxlQiCMihojUtw7d2TEY6h5LVVaYhifWXvVPUh8jgMphUbFy6jR~Tsff8HKnveg~4vyLEdmU5gX95lCTdlSuUywizLbOO6luhSC~lGIRVHoeFwCFmEQzHD3anmCLkIF2u-wJrWmT1k62A0RtirzKZ7XnLDusFGS1p3irw5-mSt7U-qCVHag__'/>
-              <div className='about-section-collector-2'>
-                <h2 className='about-section-h2'>Разработка сайтов</h2>
-                <p className='about-section-p'>Делаем классные интернет магазины, CRM, лэндинги и различные тематические сайты</p>
-              </div>
-            </div>
-            <div className='about-section-collector-1'>
-              <img className='about-section-icon' src='https://s3-alpha-sig.figma.com/img/82d7/7dc2/608c4b6f05a34699cff4b8f408594019?Expires=1713744000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=F9H5qiQUn6-k1ophUDzvmYqczlJPr93x6uflYd9AZNpvm-0gAOmWrmGY0RU2kfLwOxxY60qy0-Paje2tKcNHXpF-f7T6teOeWTW7OD-aWQYuA389crxkBgm9gygQFXGiiLEqrSXApQHyOtn9hEVsf4JuMCyBBH1G6IL01imjyAxCEbqydMeXdRfJAVfKCpjkEIi~8FqXnulvkU~Dg1p05vUeK35zTtAMdC1GAhrubPSVYFD4k6IFjY5en4QwPj~hFK4lTx7lheB0w7yBWHpJPv5w0n-~T0oYhjlGTdXAt~tLc9EvoQDY8acaN9vO2NuSv21XVnJCw7zSadBYkfU3AQ__'/>
-              <div className='about-section-collector-2'>
-                <h2 className='about-section-h2'>Разработка дизайна</h2>
-                <p className='about-section-p'>Создадим современный и уникальный дизайн для вашего веб-сайта</p>
-              </div>
-            </div>
-            <div className='about-section-collector-1'>
-              <img className='about-section-icon' src='https://s3-alpha-sig.figma.com/img/8d5e/2c84/00322b8f6a4b43c1ca4258a189d401a8?Expires=1713744000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=he8h6FTzg75D7HWE42wlMeNh62dw4zbx8gatNnJLjvTODQKPkWkjG8CuynVugYSZXeJDf123yfHo1v-JttMXnx~IwJ8FLwpd4f0YgEDDTRI6fxJgi3i9qGrA2aDD8mAmgqLsITC~jQsv~6KcEccN6AMD1b2jEZmOb8dskN83rsxldKk-sSX-p11D8nulLzhzlvm-xeYYtI2mn-eD4iETg9eATiweIjevlm1x3fxXMSuPvrhsilwUfs8V0uzgpN3khcXiz~e4tYmRmDlaGnTzoX~96eaEbUO~VG38mkp7f6fHyTmwds74un0P-9xllWOJSD4VTAITHPbrLoCG32NPzw__'/>
-              <div className='about-section-collector-2'>
-                <h2 className='about-section-h2'>Таргет-реклама Instagram</h2>
-                <p className='about-section-p'>Запустим таргетированную рекламу в Instagram + Facebook для охвата вашей аудитории</p>
-              </div>
-            </div>
-          </div>
-          <div className='about-section-row-2'>
-            <div className='about-section-collector-1'>
-              <img className='about-section-icon' src='https://s3-alpha-sig.figma.com/img/19d4/b4f7/66a1c104e8c7b5ee06b612880fab9151?Expires=1713744000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ZYMp75wJ8TeNpO3~4hbguAaLsXUEGOWqRg0ozGokONqV5FrwTZDQCOyGUYpqjmbqwSlKb35MaVw6r2Pgpr3CM1HYoksgH3digUZpw-RZfo168BttQNLMz-U1epYWDLu4m3cDZnP3sQjmlym6tt1WAfZyjQt-B~WPl-e80grXsHxAxeqyx2Ar6VC--zh373-7o1fnSKqjxOxhQVmxgGCIGcZCpo7qMY1SOEAfxJJp0TcoDI93MvpWs~pWfaJvCquFw2QjOVy6Os3XN6ae~uantUbIZQqWHRfsquuE8g0EiyqfZnCQHsSdnGsJrKncElQBmmCVP~p67RJzqkKTIfL54Q__'/>
-              <div className='about-section-collector-2'>
-                <h2 className='about-section-h2'>Продвижение сайтов</h2>
-                <p className='about-section-p'>Интернет-реклама в Google и Яндекс с помощью Google Ads и Яндекс.Директ</p>
-              </div>
-            </div>
-            <div className='about-section-collector-1'>
-              <img className='about-section-icon' src='https://s3-alpha-sig.figma.com/img/5061/0549/b17224cbb092df0b8dadf1dd405f4752?Expires=1713744000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=dFICQp1hNfOpi5LEFUPGQ9utZWRbufA3ZOtMKJ3oSrlWB~hGZ~HYv4zftIO41y8PxGhvnWvd7oCHOzT6e~u38iB2k5WqCU-xP~euz9iR7V5k6F0T12ZLbgiMfDH5UwXEHqY1oIKRbgMFcSys1f1D4hXwQstHbXsPOnPP~SFcx1ysPKiIVPhtODj47t0VRA1mxjnRgkWwa6r4YZbNMvIhwhbnAfcMZbD4lLx2JOJkOpPbSPW5sPNo4DV~0z1~-kxCEY4yc7BP7M7FqGbTeQs5PjcSnpyMH0Lc~dZmXnNoHsMmlOECTKzPHiKrZU1fo~Dh0N6wz0xhKifI-KEydgHU7A__'/>
-              <div className='about-section-collector-2'>
-                <h2 className='about-section-h2'>Поисковая оптимизация</h2>
-                <p className='about-section-p'>Полная настройка SEO-стороны вашего веб-сайта, а также его выведение на топы поиска</p>
-              </div>
-            </div>
-            <div className='about-section-collector-1'>
-              <img className='about-section-icon' src='https://s3-alpha-sig.figma.com/img/3dc3/c46b/47e42b47e3dfc27e5c234b170e00d43c?Expires=1713744000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=O62zl6DZBumT7-tvrLdToxR03c7-k5j6E1YDehoHRR2Xv~~CmB0u3VUcXLwWw~r9cr5eFNTiIV6XhgT1clhc9pXgVy~MMaa6PJ8AueYsiabYt7seyCACXznRJQhQ9N57gGMTnRnznMN1yzF~DDPQq5vSRF5oxfDHhIT~4l~45-J77vv3xDvHXzO~9oly1ckphdam-keIMi9wP3D4dbqjfQ2YfSgeeBK2xBXjdJZU5LZ70ZP3qcq-0UeLrQiQGiyPgglOzD5MjRagLzpryI4LrtgPLb0-6mkAbaHAukGBpJGkvHDMPs7LR4fYV2QpUbXHxH1j6YmZPCcFOVdZWJMF~A__'/>
-              <div className='about-section-collector-2'>
-                <h2 className='about-section-h2'>Разработка ТЗ</h2>
-                <p className='about-section-p'>Разработаем грамотное техническое задание, полностью обговаривая с вами каждый пункт</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <AboutSection />
+      
     </div>
   );
 }
---
+++ src/components/AboutSection/AboutSection.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import style from './AboutSection.module.css'
+
+const AboutSection = () => {
+  return (
+    <div className={style.AboutSection}>
+  <div className={style.Content}>
+  <h1 className={style.H1}>KWS Studio</h1>
+  <p className={style.Description}>
+    Делаем красивые дизайны для бизнеса, разрабатываем сайты, выводим бренды на рынок. <br /> Наша компания предлагает Вам следующие услуги:
+  </p>
+  <div className={style.Row1}>
+    <div className={style.Collector1}>
+      <img className={style.Icon} src='https://s3-alpha-sig.figma.com/img/ec62/1446/941485328270f1bc1016cc7f486ba5d8?Expires=1713744000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=H5I4azuA0T68KgTyOBMEZLy45jeBvPs6q3USlXaInPdDZHzzaq8BQ3lgCqkg1H6aPIGJYhUayMOTmjoWmotfoI2ToGWXgOqIegoADugwigRdy2wNCb5beWZlW4Db93GtFXnU4GxdHbsr3CZcKWFkgp48uwMyV1Lm-W5xxlQiCMihojUtw7d2TEY6h5LVVaYhifWXvVPUh8jgMphUbFy6jR~Tsff8HKnveg~4vyLEdmU5gX95lCTdlSuUywizLbOO6luhSC~lGIRVHoeFwCFmEQzHD3anmCLkIF2u-wJrWmT1k62A0RtirzKZ7XnLDusFGS1p3irw5-mSt7U-qCVHag__'/>
+      <div className={style.Collector2}>
+        <h2 className={style.H2}>Разработка сайтов</h2>
+        <p className={style.P}>Делаем классные интернет магазины, CRM, лэндинги и различные тематические сайты</p>
+      </div>
+    </div>
+    <div className={style.Collector1}>
+      <img className={style.Icon} src='https://s3-alpha-sig.figma.com/img/82d7/7dc2/608c4b6f05a34699cff4b8f408594019?Expires=1713744000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=F9H5qiQUn6-k1ophUDzvmYqczlJPr93x6uflYd9AZNpvm-0gAOmWrmGY0RU2kfLwOxxY60qy0-Paje2tKcNHXpF-f7T6teOeWTW7OD-aWQYuA389crxkBgm9gygQFXGiiLEqrSXApQHyOtn9hEVsf4JuMCyBBH1G6IL01imjyAxCEbqydMeXdRfJAVfKCpjkEIi~8FqXnulvkU~Dg1p05vUeK35zTtAMdC1GAhrubPSVYFD4k6IFjY5en4QwPj~hFK4lTx7lheB0w7yBWHpJPv5w0n-~T0oYhjlGTdXAt~tLc9EvoQDY8acaN9vO2NuSv21XVnJCw7zSadBYkfU3AQ__'/>
+      <div className={style.Collector2}>
+        <h2 className={style.H2}>Разработка дизайна</h2>
+        <p className={style.P}>Создадим современный и уникальный дизайн для вашего веб-сайта</p>
+      </div>
+    </div>
+    <div className={style.Collector1}>
+      <img className={style.Icon} src='https://s3-alpha-sig.figma.com/img/8d5e/2c84/00322b8f6a4b43c1ca4258a189d401a8?Expires=1713744000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=he8h6FTzg75D7HWE42wlMeNh62dw4zbx8gatNnJLjvTODQKPkWkjG8CuynVugYSZXeJDf123yfHo1v-JttMXnx~IwJ8FLwpd4f0YgEDDTRI6fxJgi3i9qGrA2aDD8mAmgqLsITC~jQsv~6KcEccN6AMD1b2jEZmOb8dskN83rsxldKk-sSX-p11D8nulLzhzlvm-xeYYtI2mn-eD4iETg9eATiweIjevlm1x3fxXMSuPvrhsilwUfs8V0uzgpN3khcXiz~e4tYmRmDlaGnTzoX~96eaEbUO~VG38mkp7f6fHyTmwds74un0P-9xllWOJSD4VTAITHPbrLoCG32NPzw__'/>
+      <div className={style.Collector2}>
+        <h2 className={style.H2}>Таргет-реклама Instagram</h2>
+        <p className={style.P}>Запустим таргетированную рекламу в Instagram + Facebook для охвата вашей аудитории</p>
+      </div>
+    </div>
+  </div>
+  <div className={style.Row2}>
+    <div className={style.Collector1}>
+      <img className={style.Icon} src='https://s3-alpha-sig.figma.com/img/19d4/b4f7/66a1c104e8c7b5ee06b612880fab9151?Expires=1713744000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ZYMp75wJ8TeNpO3~4hbguAaLsXUEGOWqRg0ozGokONqV5FrwTZDQCOyGUYpqjmbqwSlKb35MaVw6r2Pgpr3CM1HYoksgH3digUZpw-RZfo168BttQNLMz-U1epYWDLu4m3cDZnP3sQjmlym6tt1WAfZyjQt-B~WPl-e80grXsHxAxeqyx2Ar6VC--zh373-7o1fnSKqjxOxhQVmxgGCIGcZCpo7qMY1SOEAfxJJp0TcoDI93MvpWs~pWfaJvCquFw2QjOVy6Os3XN6ae~uantUbIZQqWHRfsquuE8g0EiyqfZnCQHsSdnGsJrKncElQBmmCVP~p67RJzqkKTIfL54Q__'/>
+      <div className={style.Collector2}>
+        <h2 className={style.H2}>Продвижение сайтов</h2>
+        <p className={style.P}>Интернет-реклама в Google и Яндекс с помощью Google Ads и Яндекс.Директ</p>
+      </div>
+    </div>
+    <div className={style.Collector1}>
+      <img className={style.Icon} src='https://s3-alpha-sig.figma.com/img/5061/0549/b17224cbb092df0b8dadf1dd405f4752?Expires=1713744000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=dFICQp1hNfOpi5LEFUPGQ9utZWRbufA3ZOtMKJ3oSrlWB~hGZ~HYv4zftIO41y8PxGhvnWvd7oCHOzT6e~u38iB2k5WqCU-xP~euz9iR7V5k6F0T12ZLbgiMfDH5UwXEHqY1oIKRbgMFcSys1f1D4hXwQstHbXsPOnPP~SFcx1ysPKiIVPhtODj47t0VRA1mxjnRgkWwa6r4YZbNMvIhwhbnAfcMZbD4lLx2JOJkOpPbSPW5sPNo4DV~0z1~-kxCEY4yc7BP7M7FqGbTeQs5PjcSnpyMH0Lc~dZmXnNoHsMmlOECTKzPHiKrZU1fo~Dh0N6wz0xhKifI-KEydgHU7A__'/>
+      <div className={style.Collector2}>
+        <h2 className={style.H2}>Поисковая оптимизация</h2>
+        <p className={style.P}>Полная настройка SEO-стороны вашего веб-сайта, а также его выведение на топы поиска</p>
+      </div>
+    </div>
+    <div className={style.Collector1}>
+      <img className={style.Icon} src='https://s3-alpha-sig.figma.com/img/3dc3/c46b/47e42b47e3dfc27e5c234b170e00d43c?Expires=1713744000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=O62zl6DZBumT7-tvrLdToxR03c7-k5j6E1YDehoHRR2Xv~~CmB0u3VUcXLwWw~r9cr5eFNTiIV6XhgT1clhc9pXgVy~MMaa6PJ8AueYsiabYt7seyCACXznRJQhQ9N57gGMTnRnznMN1yzF~DDPQq5vSRF5oxfDHhIT~4l~45-J77vv3xDvHXzO~9oly1ckphdam-keIMi9wP3D4dbqjfQ2YfSgeeBK2xBXjdJZU5LZ70ZP3qcq-0UeLrQiQGiyPgglOzD5MjRagLzpryI4LrtgPLb0-6mkAbaHAukGBpJGkvHDMPs7LR4fYV2QpUbXHxH1j6YmZPCcFOVdZWJMF~A__'/>
+      <div className={style.Collector2}>
+        <h2 className={style.H2}>Разработка ТЗ</h2>
+        <p className={style.P}>Разработаем грамотное техническое задание, полностью обговаривая с вами каждый пункт</p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+  )
+}
+
+export default AboutSection
