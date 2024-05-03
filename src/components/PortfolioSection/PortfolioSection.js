2e17b48848b564d1903a38d3b3b72460ff11750b
neyahray <fizy1688@gmail.com>
2024-05-03 18:14:09 +0600

    icons on portfolio

--- src/components/PortfolioSection/PortfolioSection.js
+++ src/components/PortfolioSection/PortfolioSection.js
@@ -1,21 +1,95 @@
 import React from 'react'
 import style from './PortfolioSection.module.css'
+import image from "../../assets/Group\ 27.png"
+import image2 from "../../assets/Group\ 28.png"
 
 const PortfolioSection = () => {
   return (
     <div className={style.PortfolioSection}>
       <h1 className={style.H1}>Портфолио</h1>
       <div className={style.collector1}>
-        <img className={style.image} src='https://s3-alpha-sig.figma.com/img/c51d/dab1/b86e6a4243b28e6969c9ead84d8298bb?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=YncLojGHxetqnMnwSqOvIXvQDJunh-N22TmRqaftT1175LA7Y0hrm4vtPE94KUygrU3rUWIjqklWWiV0KzJQO2yaz7yJja0bJn9HOTowx6tm8QEHZPO~7ZtfogCpHTgTDdmr5RM1v4AC6e2ZoqFPCsuzj9NE-MYy6o8zasF5o5KFzqhJ3W5BOivGTTVlPVORL4-sSnzpWPd9mJnDMKVDv5RSutDxCavPZtxbqV38~RORkHxwxjifHYl5J8RedAG-utkhS4Gkeza96U5bbJbnFL0deAu71daW-52FqTdpiI~fuXiWx0fE7F9Iv~mNK7LZTHcbTzfcoLoWyJbyrkJg1A__'/>
-        <img className={style.image} src='https://s3-alpha-sig.figma.com/img/8d03/8b7a/10b31c60f6b10561b0dfaca7480b34c2?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=n1CF04~oGNXcZ3jdlObhQaX9Z19KXGYe92yRHPDxFo84DZ275ymi0uTBqn6~LxAZvAk5JoEs5Z69cNk045KY3DYS4K7e6MW8gkTXB-cDWeVOglGM863t8aIF6Q78NSQVdL680OXSbQlUsjLZn8UZvKvi~sLUdF1gtR3qoZHno7048yokeHahxJHQJtc2-KpBuYpl0vwe0W46wUY4Pd4~RN0DhEJ2IDcgWMPYsnP~6znLrAOKdSPPicBq-2ixtzao6lp8tIWIrtUVG5bHK2JUhtvWnlDs4-xk5kbccOIGVDA-lv30M7Z5F9Cma~cgWK47WhAxeh8fq1FCtycb5WvYHA__'/>
-        <img className={style.image} src='https://s3-alpha-sig.figma.com/img/bae4/2b9d/cb6ad8d9927c83249259a4c813bbdd1d?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cZLXh0ezASUxsgJy-fNmoWtsE4P3lZ5EPAU5X3k5c8xkZEQCRW-vCVmnbpSIPwAVcsMskrvXu3hnTtxPg~tGfiT6IhR~BbUs23H58vCFcKR7-5S4Rno4xUtIxnqWgaQXqh2ZZHu2cU6ja2OU8BIoZh7zYQxuVi5ik3AjWmt-InIquIJBVqU-jSJZxp9GhgTrwCO8gZxIl3T2My-BhtULvsC8SQm49nVWESd2KsQ6HovFlu0dsxSjK0YNHPOU~mPoVcNx3kCYxcqoo6EuTiKqbi7vafR8KkUBYZN5FEwhz9uhUMfr~1iIXUJe8NWOiORmRz5diyFBuVzgRguQAkjfxw__'/>
-        <img className={style.image} src='https://s3-alpha-sig.figma.com/img/c6db/852c/c101d3c5c76f28c6d537c2803bb31690?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=o1DcR06TNfojMa1Y3VOcqQOqV-UyZjISQ0QRFrXVnozuJ8LP9Saop1xlVrElar-MUhhCyk599rp7ZAnrP~hcl5veVI5G6mZrpPK9f7vUcNKU5xDbAn~YuYVghvblhJ8PrtrN-sm1TNiUOe2UkJ6E6ejv8-Ri43xEQuRP0BW73qZWU5BMu~Dz~ZVB6y3u~g7JPdW0VW5WyF6-SQt5Q8LNJUZMXqooUMLLzVOEk3XMXUz3azFIYwqUrtOPLQ8~CcUE9rH26HjNAWDagpSsUyloekyOiIqnUzKI1P9HnG0bxEetx~TYaGsZySJXAJP3eQMyHjQ2Z8K12rziGeXbV0Fl8g__'/>
+        <div className={style.imageBox}>
+          <img className={style.image} src='https://s3-alpha-sig.figma.com/img/c51d/dab1/b86e6a4243b28e6969c9ead84d8298bb?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=YncLojGHxetqnMnwSqOvIXvQDJunh-N22TmRqaftT1175LA7Y0hrm4vtPE94KUygrU3rUWIjqklWWiV0KzJQO2yaz7yJja0bJn9HOTowx6tm8QEHZPO~7ZtfogCpHTgTDdmr5RM1v4AC6e2ZoqFPCsuzj9NE-MYy6o8zasF5o5KFzqhJ3W5BOivGTTVlPVORL4-sSnzpWPd9mJnDMKVDv5RSutDxCavPZtxbqV38~RORkHxwxjifHYl5J8RedAG-utkhS4Gkeza96U5bbJbnFL0deAu71daW-52FqTdpiI~fuXiWx0fE7F9Iv~mNK7LZTHcbTzfcoLoWyJbyrkJg1A__'/>
+          <div className={style.Box1}>
+            <img className={style.imageInner}
+             src={image}
+           
+            />
+          </div>
+        </div>
+        <div className={style.imageBox}>
+          <img className={style.image} src='https://s3-alpha-sig.figma.com/img/8d03/8b7a/10b31c60f6b10561b0dfaca7480b34c2?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=n1CF04~oGNXcZ3jdlObhQaX9Z19KXGYe92yRHPDxFo84DZ275ymi0uTBqn6~LxAZvAk5JoEs5Z69cNk045KY3DYS4K7e6MW8gkTXB-cDWeVOglGM863t8aIF6Q78NSQVdL680OXSbQlUsjLZn8UZvKvi~sLUdF1gtR3qoZHno7048yokeHahxJHQJtc2-KpBuYpl0vwe0W46wUY4Pd4~RN0DhEJ2IDcgWMPYsnP~6znLrAOKdSPPicBq-2ixtzao6lp8tIWIrtUVG5bHK2JUhtvWnlDs4-xk5kbccOIGVDA-lv30M7Z5F9Cma~cgWK47WhAxeh8fq1FCtycb5WvYHA__'/>
+          <div className={style.Box1}>
+            <img className={style.imageInner}
+              src={image}
+              width={85} 
+              height={85}
+              />
+          </div>
+        </div>
+        <div className={style.imageBox}>
+          <img className={style.image} src='https://s3-alpha-sig.figma.com/img/bae4/2b9d/cb6ad8d9927c83249259a4c813bbdd1d?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cZLXh0ezASUxsgJy-fNmoWtsE4P3lZ5EPAU5X3k5c8xkZEQCRW-vCVmnbpSIPwAVcsMskrvXu3hnTtxPg~tGfiT6IhR~BbUs23H58vCFcKR7-5S4Rno4xUtIxnqWgaQXqh2ZZHu2cU6ja2OU8BIoZh7zYQxuVi5ik3AjWmt-InIquIJBVqU-jSJZxp9GhgTrwCO8gZxIl3T2My-BhtULvsC8SQm49nVWESd2KsQ6HovFlu0dsxSjK0YNHPOU~mPoVcNx3kCYxcqoo6EuTiKqbi7vafR8KkUBYZN5FEwhz9uhUMfr~1iIXUJe8NWOiORmRz5diyFBuVzgRguQAkjfxw__'/>
+          <div className={style.Box1}>
+            <img className={style.imageInner}
+                src={image}
+                width={85} 
+                height={85}
+                />
+          </div>
+        </div>
+        <div className={style.imageBox}>
+          <img className={style.image} src='https://s3-alpha-sig.figma.com/img/c6db/852c/c101d3c5c76f28c6d537c2803bb31690?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=o1DcR06TNfojMa1Y3VOcqQOqV-UyZjISQ0QRFrXVnozuJ8LP9Saop1xlVrElar-MUhhCyk599rp7ZAnrP~hcl5veVI5G6mZrpPK9f7vUcNKU5xDbAn~YuYVghvblhJ8PrtrN-sm1TNiUOe2UkJ6E6ejv8-Ri43xEQuRP0BW73qZWU5BMu~Dz~ZVB6y3u~g7JPdW0VW5WyF6-SQt5Q8LNJUZMXqooUMLLzVOEk3XMXUz3azFIYwqUrtOPLQ8~CcUE9rH26HjNAWDagpSsUyloekyOiIqnUzKI1P9HnG0bxEetx~TYaGsZySJXAJP3eQMyHjQ2Z8K12rziGeXbV0Fl8g__'/>
+          <div className={style.Box1}>
+            <img className={style.imageInner}
+                src={image}
+                width={85} 
+                height={85}
+                />
+          </div>
+        </div>
       </div>
       <div className={style.collector2}>
-        <img className={style.image} src='https://s3-alpha-sig.figma.com/img/1879/a632/995d6684f04caa5221c2568fc6e55d19?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=XymrU5ZMnnaRMVtev45lGf97JFbYAE~ZicId6pXRNKHzzJKrAqGjsuMlwWyRdrNOi~uKIZnv67yPnHaP0m8WZ8k8FWer4TSIX7Rpau7hlkeZMXonaRJpgSzwcYrcf5XwstSSWKFKWcZQR7lP36wdCI2UQoDRhuHbF5-U5cL2E4fbrz3spQQopiD8rt0k~ZPJ0XBefQqpu6BPxeLoXIuM4wtGyPXm-wc2x6nudrd~~fWhlgYqw-B~t3JXsZVX4uVNiB0Rrsa0ykYxv1UjJO9EoaG4mo~8u62spSsiLo-1kJ-u19bV6f39pnipSpOly-D1O1Xe7xpIuAuQ0yFbbcNJwA__'/>
-        <img className={style.image} src='https://s3-alpha-sig.figma.com/img/4f63/dbce/d2676705704acab6d8d1163b477d0390?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=kk0SiLzC33~Lo2j3n5efxwBPmuT8gHXi-bfYSznEEfHJ0waDxUGRaks0MQ8jnM56cbJkPhksbdCyYCcd8vP~qljWHcrhH8NNkDD1acl~hGS9HXO3jzFsWk2lxaSJuFyHc3OZEae12ce2Q1fnhjW7gk9SMe1kZKXH6Pto~3kJYu95sVQ928gaWAuDm5nB5TnqzN1NFZnLtnR703aUslakum2CoTps8h2t-0JdX~ez4v7~PkT0tPlr0QX937AeE1DesPSC4QM-MXv2ESp-lq6I7An7P8Gqtv9yr1oz52jcLGNsf-jvmaxPWb2QsVTWBN4lyd1F-Ksd86vtbZsJsNJB1w__'/>
-        <img className={style.image} src='https://s3-alpha-sig.figma.com/img/4db2/7cff/b3a6c870093c3d0a650d84396683b37b?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=dYLGC19rOyAjxsmtFvKrSXhE0QJaWeldH~BLFvXq~hge8xfNujIRHMs39D2qpbXtP4U-nQoYs~Ojim3hUie0uq57gghFbcwuk6iR1v1HSEwSbshA7hYqICgC2cyH3yy4h2SG9OLtFZTlBgYYrm3JVCqQJjRNNFA4JLNjTOTk5CDc3oZ9dER0CpA-KROrZALzdEMRvy8U5~5iROJg4ON54hnVxvsr3lB6Od-5xATHwQ7r3R80uKsYCS4ORYk3kh3BezGHKfGouP96P9Aa9wedIABLgsLGZfWbKm4~cupuya727ugVUxHxn1FiZouL-q6QLMSztBMYP5UhQZ2fF1nn3A__'/>
-        <img className={style.image} src='https://s3-alpha-sig.figma.com/img/72ad/6ecd/a9592b8cd70233aea7e75c6447b806d1?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=DIQWVYVCnV~Ph7iy5SZDPs5jkXnwbmKUQl3MUJWhwnNj~nF~7Mv0pqS4oWcLVnNsuJg~RdXQEC6YR7WMWdTwvFHJIpVxshoetKBqpoycPz3FtHhLAvRmonrF2wN0Eq3BE0IgGWnmevyk3RIir0o6llpBzvbJzu247kGVKcTBT0i8fOokRUHFfoIJxTLQLzE1AnUjLKERGF1I5q3LNOuqT9n2Xvs1ztXDs-MemalV6UDN9FwU3bv~b-znyIBbELByhmedQAeNg8gt6M4KwMZ4YpnxExkIXsJARzZPODo-XwsZO1utptCTNE-cKLJ9cUdLt0cXEwZ8bjNX12eY6hXXJA__'/>
+        <div className={style.imageBox}>
+          <img className={style.image} src='https://s3-alpha-sig.figma.com/img/1879/a632/995d6684f04caa5221c2568fc6e55d19?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=XymrU5ZMnnaRMVtev45lGf97JFbYAE~ZicId6pXRNKHzzJKrAqGjsuMlwWyRdrNOi~uKIZnv67yPnHaP0m8WZ8k8FWer4TSIX7Rpau7hlkeZMXonaRJpgSzwcYrcf5XwstSSWKFKWcZQR7lP36wdCI2UQoDRhuHbF5-U5cL2E4fbrz3spQQopiD8rt0k~ZPJ0XBefQqpu6BPxeLoXIuM4wtGyPXm-wc2x6nudrd~~fWhlgYqw-B~t3JXsZVX4uVNiB0Rrsa0ykYxv1UjJO9EoaG4mo~8u62spSsiLo-1kJ-u19bV6f39pnipSpOly-D1O1Xe7xpIuAuQ0yFbbcNJwA__'/>
+          <div className={style.Box1}>
+            <img className={style.imageInner}
+              src={image2}
+              width={85} 
+              height={85}
+            />
+          </div>
+        </div>
+        <div className={style.imageBox}>
+          <img className={style.image} src='https://s3-alpha-sig.figma.com/img/4f63/dbce/d2676705704acab6d8d1163b477d0390?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=kk0SiLzC33~Lo2j3n5efxwBPmuT8gHXi-bfYSznEEfHJ0waDxUGRaks0MQ8jnM56cbJkPhksbdCyYCcd8vP~qljWHcrhH8NNkDD1acl~hGS9HXO3jzFsWk2lxaSJuFyHc3OZEae12ce2Q1fnhjW7gk9SMe1kZKXH6Pto~3kJYu95sVQ928gaWAuDm5nB5TnqzN1NFZnLtnR703aUslakum2CoTps8h2t-0JdX~ez4v7~PkT0tPlr0QX937AeE1DesPSC4QM-MXv2ESp-lq6I7An7P8Gqtv9yr1oz52jcLGNsf-jvmaxPWb2QsVTWBN4lyd1F-Ksd86vtbZsJsNJB1w__'/>
+          <div className={style.Box1}>
+            <img className={style.imageInner}
+              src={image2}
+              width={85} 
+              height={85}
+            />
+          </div>
+        </div>
+        <div className={style.imageBox}>
+          <img className={style.image} src='https://s3-alpha-sig.figma.com/img/4db2/7cff/b3a6c870093c3d0a650d84396683b37b?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=dYLGC19rOyAjxsmtFvKrSXhE0QJaWeldH~BLFvXq~hge8xfNujIRHMs39D2qpbXtP4U-nQoYs~Ojim3hUie0uq57gghFbcwuk6iR1v1HSEwSbshA7hYqICgC2cyH3yy4h2SG9OLtFZTlBgYYrm3JVCqQJjRNNFA4JLNjTOTk5CDc3oZ9dER0CpA-KROrZALzdEMRvy8U5~5iROJg4ON54hnVxvsr3lB6Od-5xATHwQ7r3R80uKsYCS4ORYk3kh3BezGHKfGouP96P9Aa9wedIABLgsLGZfWbKm4~cupuya727ugVUxHxn1FiZouL-q6QLMSztBMYP5UhQZ2fF1nn3A__'/>
+          <div className={style.Box1}>
+            <img className={style.imageInner}
+              src={image2}
+              width={85} 
+              height={85}
+            />
+          </div>
+        </div>
+        <div className={style.imageBox}>
+          <img className={style.image} src='https://s3-alpha-sig.figma.com/img/72ad/6ecd/a9592b8cd70233aea7e75c6447b806d1?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=DIQWVYVCnV~Ph7iy5SZDPs5jkXnwbmKUQl3MUJWhwnNj~nF~7Mv0pqS4oWcLVnNsuJg~RdXQEC6YR7WMWdTwvFHJIpVxshoetKBqpoycPz3FtHhLAvRmonrF2wN0Eq3BE0IgGWnmevyk3RIir0o6llpBzvbJzu247kGVKcTBT0i8fOokRUHFfoIJxTLQLzE1AnUjLKERGF1I5q3LNOuqT9n2Xvs1ztXDs-MemalV6UDN9FwU3bv~b-znyIBbELByhmedQAeNg8gt6M4KwMZ4YpnxExkIXsJARzZPODo-XwsZO1utptCTNE-cKLJ9cUdLt0cXEwZ8bjNX12eY6hXXJA__'/>
+          <div className={style.Box1}>
+            <img className={style.imageInner}
+              src={image2}
+              width={85} 
+              height={85}
+            />
+          </div>
+        </div>
+
       </div>
     </div>
   )
